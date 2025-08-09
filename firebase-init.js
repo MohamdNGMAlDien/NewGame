bb51263014a0b833a1fb545a4f0317048595acd3
MohamdNGMAlDien <154337863+MohamdNGMAlDien@users.noreply.github.com>
2025-08-09 21:49:07 +0200

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,1 +1,14 @@
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyB406N93ZsWzxq6bk69DCn-Elkcr-KJEoc",
+  authDomain: "dogleap-9607c.firebaseapp.com",
+  projectId: "dogleap-9607c",
+  storageBucket: "dogleap-9607c.firebasestorage.app",
+  messagingSenderId: "569606406924",
+  appId: "1:569606406924:web:d2cc06de55171f3f007f29",
+  measurementId: "G-ZMX41F48D4"
+};
+const app = initializeApp(firebaseConfig);
+export { app };
 
