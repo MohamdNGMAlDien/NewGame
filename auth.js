cd17118a8cee974ecd72fb8b36330336acce5cc1
MohamdNGMAlDien <154337863+MohamdNGMAlDien@users.noreply.github.com>
2025-08-09 21:49:45 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,30 @@
+// auth.js
+import { app } from "./firebase-init.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInAnonymously
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+window.loginWithGoogle = function () {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      alert("مرحبًا: " + result.user.displayName);
+      window.location.href = "game.html";
+    })
+    .catch((error) => alert(error.message));
+};
+
+window.loginAsGuest = function () {
+  signInAnonymously(auth)
+    .then(() => {
+      alert("تم الدخول كضيف");
+      window.location.href = "game.html";
+    })
+    .catch((error) => alert(error.message));
+};
 
